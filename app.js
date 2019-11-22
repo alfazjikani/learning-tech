5da20274193fb952740f47a8ba934f963bb536fe
Alfaz Jikani <alfazjikani@gmail.com>
2019-11-22 23:32:59 +0530

    url added for cloud.

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ app.use(helmet());
 
 // initializing database connection
 var mongoose = require('mongoose');
-var mongoDBUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/student_management';
+
+var mongoDBCloudUrl = 'mongodb+srv://root:root@cluster0-fcizl.mongodb.net/student_management?retryWrites=true&w=majority';
+var mongoDBOnpremiseUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/student_management';
+var mongoDBUrl = process.env.MONGODB_URI || mongoDBCloudUrl || mongoDBOnpremiseUrl;
+
 mongoose.connect(mongoDBUrl, {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection failed!!'));
--- scripts/create_user.js
+++ scripts/create_user.js
@@ -2,7 +2,9 @@ var User = require('../models/user');
 
 // initializing database connection
 var mongoose = require('mongoose');
-var mongoDBUrl = 'mongodb://localhost:27017/student_management';
+var mongoDBCloudUrl = 'mongodb+srv://root:root@cluster0-fcizl.mongodb.net/student_management?retryWrites=true&w=majority';
+var mongoDBOnpremiseUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/student_management';
+var mongoDBUrl = process.env.MONGODB_URI || mongoDBCloudUrl || mongoDBOnpremiseUrl;
 mongoose.connect(mongoDBUrl, {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection failed!!'));
